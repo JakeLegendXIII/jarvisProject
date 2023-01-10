9f3883f8a5cd7026487e7ff1de009b5088ff7669
Jake Kirkbride <jkirkbride@rjtechnologies.com>
2023-01-10 10:48:14 -0500

    Getting messages from prompts

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import os
 import openai as ai
 
-ai.organization = 'org-cBExy83sUBuAY7BRECeNb9QB'
+ai.organization = 'org-2J6BFEzhs2n3BzlyPa2caWQE'
 ai.api_key = os.environ.get('API_KEY')
 
-# Press the green button in the gutter to run the script.
+def query_ai(prompt):
+    print('Prompt:',prompt)
+    completions = ai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=128,
+        n=1,
+        stop=None,
+        temperature=0.5
+    )
+    message = completions.choices[0].text
+    print('Message: ',message)
+    return message
+
+
 if __name__ == '__main__':
+    query_ai('How long would it take to get to Mars?')
     print('It Works!')
 
 
