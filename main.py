97632da4d8028a676b6d133c0ace210d33a9c768
Jake Kirkbride <jkirkbride@rjtechnologies.com>
2023-01-10 10:22:36 -0500

    Adding API_KEY env var

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
+import openai as ai
 
+ai.organization = 'org-cBExy83sUBuAY7BRECeNb9QB'
+ai.api_key = os.environ.get('API_KEY')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
